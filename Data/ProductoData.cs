794a0f7f6050fa0147fb019907f2803d761c448b
TomasGarceron <tomasgarceron@gmail.com>
2024-06-05 01:50:48 -0300

    Proyecto Inicial

---
+++ Data/ProductoData.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models.Entidades;
+
+namespace Data
+{
+    public static class ProductoData
+    {
+        public static List<Producto> listaProducto = new List<Producto>{
+            new Producto { Id=1 , NombreProducto= "Producto1", Categoria= "Computadoras", Marca= "HP", Precio=2000},
+            new Producto { Id=2 , NombreProducto= "Producto2", Categoria= "Computadoras", Marca= "Windows", Precio=2200},
+            new Producto { Id=3 , NombreProducto= "Producto3", Categoria= "Computadoras", Marca= "Lg", Precio=2350},
+            new Producto { Id=4 , NombreProducto= "Producto4", Categoria= "Computadoras", Marca= "Samsung", Precio=2000},
+            new Producto { Id=5 , NombreProducto= "Producto5", Categoria= "Computadoras", Marca= "Asus", Precio=2450},
+        };
+    }
+}
