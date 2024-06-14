80fdf4b8ba88250b4f09d52f79d8e4a5b3ec8b22
TomasGarceron <tomasgarceron@gmail.com>
2024-06-14 02:04:29 -0300

    agregar script de inserts de tabla productos

---
+++ Scripts/14062024.sql
@@ -0,0 +1,5 @@
+INSERT into Producto (NombreProducto, CategoriaId, MarcaId, Precio, Costo)
+VALUES ('Hp -Envy', 1, 1, 2000, 1500);
+
+INSERT into Producto (NombreProducto, CategoriaId, MarcaId, Precio, Costo)
+VALUES ('Hp -EliteBook', 1, 1, 2500, 1800);
